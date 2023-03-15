27bf4f73aad5c9f2ad81873e80db6440a8518716
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-03-14 20:18:45 -0600

    add app.js changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+
+  const [gameState, setGameState] = useState({
+    pieces: '',
+    colors: '',
+    whites_turn: true
+  })
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5555/moves')
+      .then(r => r.json())
+      .then(data => {setGameState(data[0])})
+  }, []);
+
   return (
-    <h1>First app</h1>
+    <h1>{gameState.pieces}</h1>
   );
 }
 
