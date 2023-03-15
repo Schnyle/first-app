76fb84ecea2759194bdfdd0cd21f45ed8eb3917a
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-03-14 19:43:23 -0600

    seed db to starting position

--- server/seed.py
+++ server/seed.py
@@ -3,4 +3,30 @@ from models import db, Game
 
 with app.app_context():
     Game.query.delete()
-    
+
+    starting_position = Game(
+        pieces = '''
+            RHBQKBHR
+            PPPPPPPP
+            EEEEEEEE
+            EEEEEEEE
+            EEEEEEEE
+            EEEEEEEE
+            PPPPPPPP
+            RHBQKBHR
+        ''',
+        colors = '''
+            BBBBBBBB
+            BBBBBBBB
+            EEEEEEEE
+            EEEEEEEE
+            EEEEEEEE
+            EEEEEEEE
+            WWWWWWWW
+            WWWWWWWW
+        ''',
+        whites_turn = True
+    )
+
+    db.session.add(starting_position)
+    db.session.commit()
